7a8672acec75d705bde40e4afbd6be866ebb1018
Nacocx <88483952+Nacog@users.noreply.github.com>
2026-04-04 17:36:34 +0800

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nacog.github.io/Covteam/'
+	site: 'https://nacog.github.io',
+	base: '/Covteam/'
 });
